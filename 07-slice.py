fa9cdf1e0552e01d09388f02f42167af1a8c4cae
kurozakizz <squallcope@gmail.com>
2018-12-05 20:45:56 +0700

    finish slice example

---
+++ 07-slice.py
@@ -0,0 +1,18 @@
+animals = ['man', 'bear', 'pig', 'cow', 'duck', 'horse']
+some_animals = animals[1:4]
+print('Some animals: {}'.format(some_animals))
+
+first_two = animals[0:2]
+print('First two animals: {}'.format(first_two))
+
+first_two_again = animals[:2]
+print('First two animals: {}'.format(first_two_again))
+
+last_two = animals[4:6]
+print('Last two animals: {}'.format(last_two))
+
+last_two_again = animals[-2:]
+print('Last two animals: {}'.format(last_two_again))
+
+date_string = '02/11/2018'
+print('Year: {}'.format(date_string[-4:]))
