1859dbfaf556c8dfd11bebd5da813543b6e5e8b0
kurozakizz <squallcope@gmail.com>
2018-12-05 13:41:56 +0700

    finish boolean example

---
+++ boolean.py
@@ -0,0 +1,14 @@
+first = True
+second = False
+print('{} {}'.format(first, second))
+
+print('{}'.format(37 > 26))
+print('{}'.format(37 < 26))
+
+age = 11
+if age < 15:
+    print('You will be legend')
+elif age >= 15 and age <=25 :
+    print()
+else:
+    print('You are too old to be legend')
